5782de7314cc9c214f5c5b211ebd56e48c998d91
Yohana Mayra <yohanamayra@gmail.com>
2023-02-05 21:26:00 -0300

    Calcular Anos Luz
    Signed-off-by: Yohana Mayra <yohanamayra@gmail.com>

---
+++ PrimeiraAula/Desafio2/anosLuz.js
@@ -0,0 +1,4 @@
+let anoCalcular = prompt("Insira quantos quilômetro gostaria de converter para anos-luz");
+let anosLuz = anoCalcular * 9500000000000;
+
+alert("Aproximadamente " + anosLuz + " Anos Luz");
