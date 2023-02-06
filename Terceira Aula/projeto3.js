9475dda436c6626a6e8c16a391568fe21471c811
Yohana Mayra <yohanamayra@gmail.com>
2023-02-05 21:23:44 -0300

    Terceira Aula
    Signed-off-by: Yohana Mayra <yohanamayra@gmail.com>

---
+++ Terceira Aula/projeto3.js
@@ -0,0 +1,20 @@
+// var filmeRafa = 'https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg'
+// var filmePaulo = 'https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg'
+// var filmeGui = 'https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080'
+
+
+// var listaFilmes = []
+// listaFilmes[0] = 'https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg'
+// listaFilmes[1] = 'https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg'
+// listaFilmes[2] = 'https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080'
+    
+//for (var i = 0; i < 3; i++) {
+    //document.write("<img src =" + listaFilmes[i] + ">")
+    //alert(i)
+//}
+
+var listaFilmes = ['https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg', 'https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg', 'https://musicart.xboxlive.com/7/94fc5000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080', 'https://upload.wikimedia.org/wikipedia/pt/7/76/10_Things_I_Hate_About_You.jpg', 'https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/91/08/82/20128877.JPG']
+
+for (var i = 0; i < listaFilmes.length; i++) {
+    document.write("<img src =" + listaFilmes[i] + ">")
+}
