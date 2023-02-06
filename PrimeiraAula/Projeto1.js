244bd210294a963222889bcf5abfe6712976e85e
Yohana Mayra <yohanamayra@gmail.com>
2023-02-05 21:22:40 -0300

    Primeira Aula
    Signed-off-by: Yohana Mayra <yohanamayra@gmail.com>

---
+++ PrimeiraAula/Projeto1.js
@@ -0,0 +1,8 @@
+// alert('Estamos começando a Imersão! :) ')       //Mensagem de Alerta
+
+var valorEmDollar = 30;
+var cotacaoDoDollar = 5.32;
+
+var valorEmReal = valorEmDollar * cotacaoDoDollar;
+var valorEmReal = valorEmReal.toFixed(2);
+alert("R$" + valorEmReal);
